a04f30429c10971852319bf3fde55bd3eb9dbd29
otbor <otborskyi@gmail.com>
2022-06-17 19:32:52 +0300

    /

---
+++ UtilsT.java
@@ -0,0 +1,9 @@
+public class UtilsT {
+    public static int birthdaycalculate(Dog dog){
+        return 20220- dog,getAge()
+        ;
+    }
+    public static void printWishyou ( Dog dog){
+        System.out.println(" Happy birthday my lovely pet" +dog.getName());
+    }
+}
